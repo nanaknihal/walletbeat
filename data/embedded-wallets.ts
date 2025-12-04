f9574f2f6d9d30083b2df052c457d1d4d0d3105c
Nanak Nihal Khalsa <nanaknihal@gmail.com>
2025-12-03 21:52:51 -0800

    new blank template for waap

--- data/embedded-wallets.ts
+++ data/embedded-wallets.ts
@@ -1,6 +1,8 @@
 import { rateWallet } from '@/schema/wallet'
 
 import { unratedEmbeddedTemplate } from './embedded-wallets/unrated.tmpl'
+import { waapEmbeddedTemplate } from './embedded-wallets/waap.tmpl'
 
 /** The unrated embedded wallet as a rated wallet. */
 export const unratedEmbeddedWallet = rateWallet(unratedEmbeddedTemplate)
+export const waapEmbeddedWallet = rateWallet(waapEmbeddedTemplate)
